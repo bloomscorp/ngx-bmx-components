f7994a278bd9dae815344e1afdd7a4545d1cb598
onclave <11374405+onclave@users.noreply.github.com>
2022-04-21 01:03:51 +0530

    add bmx-input text-area-input models

--- projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/text-area-input/abstract-bmx-text-area-input.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/text-area-input/abstract-bmx-text-area-input.ts
@@ -1,2 +1,23 @@
-export class AbstractBmxTextAreaInput {
+import {BmxTextAreaInput} from "../../interface/bmx-text-area-input";
+import {MatFormFieldAppearance} from "@angular/material/form-field";
+import {BmxValidator} from "../../interface/bmx-validator";
+import {BmxInputType} from "../../interface/bmx-input-type";
+
+export class AbstractBmxTextAreaInput implements BmxTextAreaInput {
+
+	public inputType: BmxInputType = BmxInputType.TEXT_AREA;
+
+	protected constructor(
+		public appearance: MatFormFieldAppearance = 'outline',
+		public disabled: boolean = false,
+		public label: string,
+		public rows: number,
+		public cols: number,
+		public hint: string,
+		public name: string,
+		public placeholder: string,
+		public readOnly: boolean = false,
+		public validators: BmxValidator[] = []
+	) {
+	}
 }
--- projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/text-area-input/bmx-text-area-input.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/text-area-input/bmx-text-area-input.ts
@@ -1,2 +1,56 @@
-export class BmxTextAreaInput {
+import {AbstractBmxTextAreaInput} from "./abstract-bmx-text-area-input";
+import {MatFormFieldAppearance} from "@angular/material/form-field";
+import {BmxSelectInputItem} from "../../interface/bmx-select-input-item";
+import {BmxValidator} from "../../interface/bmx-validator";
+
+export class BmxTextAreaInput extends AbstractBmxTextAreaInput {
+	public constructor (
+		appearance: MatFormFieldAppearance = 'outline',
+		disabled: boolean = false,
+		label: string,
+		rows: number,
+		cols: number,
+		hint: string,
+		name: string,
+		placeholder: string,
+		readOnly: boolean = false,
+		validators: BmxValidator[] = []
+	) {
+		super(
+			appearance,
+			disabled,
+			label,
+			rows,
+			cols,
+			hint,
+			name,
+			placeholder,
+			readOnly,
+			validators
+		);
+	}
+
+	public static getInstance(
+		label: string,
+		hint: string,
+		name: string,
+		placeholder: string,
+		validators: BmxValidator[] = [],
+		rows: number = 1,
+		cols: number = 50
+	): BmxTextAreaInput {
+
+		return new BmxTextAreaInput(
+			'outline',
+			false,
+			label,
+			rows,
+			cols,
+			hint,
+			name,
+			placeholder,
+			false,
+			validators
+		);
+	}
 }

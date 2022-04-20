abb9d84aaa242ccba5b7e8a7c41f1c564f842afe
onclave <11374405+onclave@users.noreply.github.com>
2022-04-21 01:01:21 +0530

    add bmx-input select-input models

--- projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/select-input/abstract-bmx-select-input.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/select-input/abstract-bmx-select-input.ts
@@ -1,2 +1,44 @@
-export class AbstractBmxSelectInput {
+import {BmxSelectInput} from "../../interface/bmx-select-input";
+import {MatFormFieldAppearance} from "@angular/material/form-field";
+import {BmxValidator} from "../../interface/bmx-validator";
+import {BmxInputType} from "../../interface/bmx-input-type";
+import {BmxSelectInputType} from "../../interface/bmx-input-element-types";
+import {BmxSelectInputExtendedItem} from "../../interface/bmx-select-input-extended-item";
+import {BmxSelectInputItem} from "../../interface/bmx-select-input-item";
+
+export class AbstractBmxSelectInput implements BmxSelectInput {
+
+	public inputType: BmxInputType = BmxInputType.SELECT;
+	public entries: BmxSelectInputExtendedItem[] = [];
+
+	protected constructor(
+		public appearance: MatFormFieldAppearance = 'outline',
+		public disabled: boolean = false,
+		public type: BmxSelectInputType,
+		public label: string,
+		public hint: string,
+		public name: string,
+		public placeholder: string,
+		public readOnly: boolean = false,
+		entries: BmxSelectInputItem[] = [],
+		public validators: BmxValidator[] = []
+	) {
+		this.entries = AbstractBmxSelectInput.mapToExtendedItems(entries);
+	}
+
+	public static mapToExtendedItems(entries: BmxSelectInputItem[]): BmxSelectInputExtendedItem[] {
+
+		const extendedEntries: BmxSelectInputExtendedItem[] = [];
+
+		entries.forEach((entry: BmxSelectInputItem, index: number) => {
+			extendedEntries.push({
+				id: index,
+				title: entry.title,
+				value: entry.value,
+				disabled: entry.disabled ? entry.disabled : false
+			});
+		});
+
+		return extendedEntries;
+	}
 }
--- projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/select-input/bmx-single-select-input.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/select-input/bmx-single-select-input.ts
@@ -1,2 +1,55 @@
-export class BmxSingleSelectInput {
+import {AbstractBmxSelectInput} from "./abstract-bmx-select-input";
+import {MatFormFieldAppearance} from "@angular/material/form-field";
+import {BmxValidator} from "../../interface/bmx-validator";
+import {BmxSelectInput} from "../../interface/bmx-select-input";
+import {BmxSelectInputExtendedItem} from "../../interface/bmx-select-input-extended-item";
+import {BmxSelectInputItem} from "../../interface/bmx-select-input-item";
+
+export class BmxSingleSelectInput extends AbstractBmxSelectInput {
+
+	public constructor(
+		appearance: MatFormFieldAppearance = 'outline',
+		disabled: boolean = false,
+		label: string,
+		hint: string,
+		name: string,
+		placeholder: string,
+		readOnly: boolean = false,
+		entries: BmxSelectInputItem[],
+		validators: BmxValidator[] = []
+	) {
+		super(
+			appearance,
+			disabled,
+			'single',
+			label,
+			hint,
+			name,
+			placeholder,
+			readOnly,
+			entries,
+			validators
+		);
+	}
+
+	public static getInstance(
+		label: string,
+		hint: string,
+		name: string,
+		placeholder: string,
+		entries: BmxSelectInputItem[],
+		validators: BmxValidator[] = []
+	): BmxSelectInput {
+		return new BmxSingleSelectInput(
+			'outline',
+			false,
+			label,
+			hint,
+			name,
+			placeholder,
+			false,
+			entries,
+			validators
+		);
+	}
 }

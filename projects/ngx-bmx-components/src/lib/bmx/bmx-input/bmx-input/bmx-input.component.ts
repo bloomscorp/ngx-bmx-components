44b8ab64a808a1c31406ee81c1f1dd5042df8c70
onclave <11374405+onclave@users.noreply.github.com>
2022-04-21 01:06:00 +0530

    init bmx-input components

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/bmx-input/bmx-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-bmx-input',
+  templateUrl: './bmx-input.component.html',
+  styleUrls: ['./bmx-input.component.css']
+})
+export class BmxInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/bmx-select-input/bmx-select-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-bmx-select-input',
+  templateUrl: './bmx-select-input.component.html',
+  styleUrls: ['./bmx-select-input.component.css']
+})
+export class BmxSelectInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

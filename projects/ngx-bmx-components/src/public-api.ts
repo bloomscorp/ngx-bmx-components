9ff755ca7d279aaa6d96343818552cf94d0b5d16
onclave <11374405+onclave@users.noreply.github.com>
2022-04-21 03:38:54 +0530

    export all models

--- projects/ngx-bmx-components/src/public-api.ts
+++ projects/ngx-bmx-components/src/public-api.ts
@@ -7,9 +7,5 @@ export * from './lib/ngx-bmx-components.component';
 export * from './lib/ngx-bmx-components.module';
 
 export * from './lib/bmx/bmx.module';
-
-// export form models
 export * from './lib/bmx/bmx-form/model/bmx-form-models';
-
-// export input models
 export * from './lib/bmx/bmx-input/model/bmx-input-models';

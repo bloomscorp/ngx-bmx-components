006ece8503b3d11031b9fb1469f5463bb0beb36e
onclave <11374405+onclave@users.noreply.github.com>
2022-04-21 03:27:26 +0530

    test export models

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-form/model/bmx-form-models.ts
@@ -0,0 +1,4 @@
+export * from './abstract-bmx-form-data';
+export * from './simple-bmx-form-data';
+export * from './trio-bmx-form-data';
+export * from './tuple-bmx-form-data';
---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/content-input/bmx-content-input-models.ts
@@ -0,0 +1,5 @@
+export * from './abstract-bmx-content-input';
+export * from './bmx-contact-input';
+export * from './bmx-email-input';
+export * from './bmx-number-input';
+export * from './bmx-text-input';

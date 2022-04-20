d4bc41f2716854cc7a1869fff576c7aa96c76863
onclave <11374405+onclave@users.noreply.github.com>
2022-04-21 01:03:03 +0530

    init bmx-input text-area-input models

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/text-area-input/abstract-bmx-text-area-input.spec.ts
@@ -0,0 +1,7 @@
+import { AbstractBmxTextAreaInput } from './abstract-bmx-text-area-input';
+
+describe('AbstractBmxTextAreaInput', () => {
+  it('should create an instance', () => {
+    expect(new AbstractBmxTextAreaInput()).toBeTruthy();
+  });
+});
---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/text-area-input/bmx-text-area-input.spec.ts
@@ -0,0 +1,7 @@
+import { BmxTextAreaInput } from './bmx-text-area-input';
+
+describe('BmxTextAreaInput', () => {
+  it('should create an instance', () => {
+    expect(new BmxTextAreaInput()).toBeTruthy();
+  });
+});

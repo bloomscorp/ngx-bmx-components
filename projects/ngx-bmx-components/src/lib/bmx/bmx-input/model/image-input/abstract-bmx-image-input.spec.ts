b6548cb560873d079ac20b0d7a91b364f96d3c65
namratapdr <namratapodder138@gmail.com>
2022-05-09 17:48:25 +0530

    simple image input

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/image-input/abstract-bmx-image-input.spec.ts
@@ -0,0 +1,7 @@
+import { AbstractBmxImageInput } from './abstract-bmx-image-input';
+
+describe('AbstractBmxImageInput', () => {
+  it('should create an instance', () => {
+    expect(new AbstractBmxImageInput()).toBeTruthy();
+  });
+});

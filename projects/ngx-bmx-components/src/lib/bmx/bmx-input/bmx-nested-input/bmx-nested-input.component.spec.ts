b406adff1f223be026bfe89eddd5c615eff0e085
namratapdr <namratapodder138@gmail.com>
2022-05-15 17:59:13 +0530

    nested input

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/bmx-nested-input/bmx-nested-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BmxNestedInputComponent } from './bmx-nested-input.component';
+
+describe('BmxNestedInputComponent', () => {
+  let component: BmxNestedInputComponent;
+  let fixture: ComponentFixture<BmxNestedInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BmxNestedInputComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BmxNestedInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

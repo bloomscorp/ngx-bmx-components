05dbb8c78e749de7d0a55625a5c19c4f6095cda7
onclave <11374405+onclave@users.noreply.github.com>
2022-05-25 23:28:45 +0530

    add bmx-button

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-button/bmx-button.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BmxButtonComponent } from './bmx-button/bmx-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    BmxButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BmxButtonModule { }
---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-button/bmx-button/bmx-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bmx-button',
+  templateUrl: './bmx-button.component.html',
+  styleUrls: ['./bmx-button.component.scss']
+})
+export class BmxButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

363b6438eb7786797bdcfcfb19be2e232f35840b
onclave <11374405+onclave@users.noreply.github.com>
2022-05-28 13:55:07 +0530

    fix bmx-form-button implementation

--- projects/ngx-bmx-components/src/lib/bmx/bmx-button/bmx-form-button/bmx-warn-form-button/bmx-warn-form-button.component.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-button/bmx-form-button/bmx-warn-form-button/bmx-warn-form-button.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {BmxFormButton} from "../interface/bmx-form-button";
 import {FormGroup} from "@angular/forms";
+import {BmxButtonElement} from "../../interface/bmx-button-element-types";
 
 @Component({
 	selector: 'bmx-warn-form-button',
@@ -10,15 +11,17 @@ import {FormGroup} from "@angular/forms";
 export class BmxWarnFormButtonComponent implements OnInit {
 
 	@Input()
-	public data: BmxFormButton = {} as BmxFormButton;
+	public data: BmxButtonElement = {} as BmxFormButton;
 
 	@Input()
 	public formGroup: FormGroup = {} as FormGroup;
+
+	public buttonData: BmxFormButton = {} as BmxFormButton;
 
 	constructor() {
 	}
 
 	ngOnInit(): void {
+		this.buttonData = <BmxFormButton>this.data;
 	}
-
 }

84e74d534d7520574f8303aed2e888ee7b54bb2e
onclave <11374405+onclave@users.noreply.github.com>
2022-06-16 19:37:46 +0530

    update bmx-info snackbar

--- projects/ngx-bmx-components/src/lib/bmx/bmx-snackbar/bmx-info-snackbar/bmx-info-snackbar.component.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-snackbar/bmx-info-snackbar/bmx-info-snackbar.component.ts
@@ -1,16 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_SNACK_BAR_DATA, MatSnackBarRef} from "@angular/material/snack-bar";
+import {BmxSnackbarComponent} from "../bmx-snackbar/bmx-snackbar.component";
+import {BmxSnackData} from "../interface/bmx-snack-data";
 
 @Component({
 	selector: 'bmx-info-snackbar',
-	templateUrl: './bmx-info-snackbar.component.html',
-	styleUrls: ['./bmx-info-snackbar.component.scss']
+	templateUrl: '../bmx-snackbar/bmx-snackbar.component.html',
+	styleUrls: ['../bmx-snackbar/bmx-snackbar.component.scss']
 })
-export class BmxInfoSnackbarComponent implements OnInit {
+export class BmxInfoSnackbarComponent extends BmxSnackbarComponent implements OnInit {
 
-	constructor() {
+	private static containerClass = 'fr-info-snack';
+	private static buttonClass = 'fr-fire-snack-info-button';
+
+	constructor(
+		private _snackbarComponentRef: MatSnackBarRef<BmxInfoSnackbarComponent>,
+		@Inject(MAT_SNACK_BAR_DATA) public override data: BmxSnackData
+	) {
+		super(_snackbarComponentRef, data);
 	}
 
 	ngOnInit(): void {
+		this.init(
+			BmxInfoSnackbarComponent.containerClass,
+			BmxInfoSnackbarComponent.buttonClass
+		);
 	}
-
 }

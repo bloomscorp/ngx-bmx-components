23eb31383206d2138e1e140f6bcdc4aa9e69794e
onclave <11374405+onclave@users.noreply.github.com>
2022-06-16 19:16:07 +0530

    add safe-html pipe

--- projects/ngx-bmx-components/src/lib/bmx/bmx-pipe/bmx-pipe.module.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-pipe/bmx-pipe.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeUrlPipe } from './safe-url.pipe';
+import { SafeHtmlPipe } from './safe-html.pipe';
 
 
 
 @NgModule({
   declarations: [
-    SafeUrlPipe
+    SafeUrlPipe,
+    SafeHtmlPipe
   ],
   imports: [
     CommonModule

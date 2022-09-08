ee36376148addcd1fe82731a12158bdcfcea1c9e
namratapdr <namratapodder138@gmail.com>
2022-09-08 13:29:05 +0530

    export password input

--- projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/content-input/bmx-content-input-models.ts
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/content-input/bmx-content-input-models.ts
@@ -3,3 +3,4 @@ export * from './bmx-contact-input';
 export * from './bmx-email-input';
 export * from './bmx-number-input';
 export * from './bmx-text-input';
+export * from './bmx-password-input';

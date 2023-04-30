e5af30f02d341da54312aa16eb8eb8179a7b8594
Deep-Dey <deepdey20998@gmail.com>
2023-04-30 17:06:49 +0530

    add richtext input

---
+++ projects/ngx-bmx-components/src/lib/bmx/bmx-input/model/richtext-input/bmx-simple-richtext-input.spec.ts
@@ -0,0 +1,7 @@
+import { BmxSimpleRichtextInput } from './bmx-simple-richtext-input';
+
+describe('BmxSimpleRichtextInput', () => {
+  it('should create an instance', () => {
+    expect(new BmxSimpleRichtextInput()).toBeTruthy();
+  });
+});
